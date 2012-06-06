374efe9bd0d95d677f784861166f254d094f9082
Bon-Yong Lee <bylee78@gmail.com>
2012-06-06 19:29:41 +0900

    modify config

--- src/web/americano/service/BookService.java
+++ src/web/americano/service/BookService.java
@@ -2,12 +2,22 @@ package americano.service;
 
 import java.util.Collection;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import escode.util.ObjectUtils;
+
+import americano.dao.BookDao;
+import americano.model.Book;
 import americano.model.Chapter;
 
 
 public class
 BookService
 {
+	@Autowired
+	protected BookDao bookDao;
+	
+	
 	public Collection<Chapter>
 	getChapters(
 		final String bookId
@@ -15,5 +25,13 @@ BookService
 	{
 		return null;
 	}
-
+	
+	public Book
+	createBook( final Book book )
+	{
+		book.setId( ObjectUtils.generateGUID( book ) );
+		bookDao.insert( book );
+		
+		return book;
+	}
 }
