cbeacb8ad2ceceda9310307c3d593f62b3b54b19
Bon-Yong Lee <bylee78@gmail.com>
2012-06-06 18:16:37 +0900

    add chapter mock

--- src/mock/americano/service/BookServiceMock.java
+++ src/mock/americano/service/BookServiceMock.java
@@ -10,15 +10,30 @@ extends BookService
 {
 	public Collection<Chapter> getChapters( String bookId )
 	{
-		final ArrayList<Chapter> chpaters = new ArrayList<Chapter>();
+		final ArrayList<Chapter> chapters = new ArrayList<Chapter>();
 		if ( "aaa".equals( bookId ) )
 		{
+			final Chapter chapter1 = new Chapter();
+			chapter1.setId( "ch1" );
+			chapter1.setTitle( "aaa-Chapter1" );
+			chapters.add( chapter1 );
+			final Chapter chapter2 = new Chapter();
+			chapter2.setId( "ch2" );
+			chapter2.setTitle( "aaa-Chapter2" );
+			chapters.add( chapter2 );
 		}
 		else if ( "bbb".equals( bookId ) )
 		{
-			
+			final Chapter chapter1 = new Chapter();
+			chapter1.setId( "ch1" );
+			chapter1.setTitle( "bbb-Chapter1" );
+			chapters.add( chapter1 );
+			final Chapter chapter2 = new Chapter();
+			chapter2.setId( "ch2" );
+			chapter2.setTitle( "bbb-Chapter2" );
+			chapters.add( chapter2 );
 		}
-		return chpaters;
+		return chapters;
 	}
 
 }
